d3f14a64dc1f73a4a561a4f1144fd5afe2e81348
Atsushi Hongo <hongo_atsushi@applibot.co.jp>
2025-06-11 15:12:22 +0900

    pwa next.js

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPWA from 'next-pwa';
 
-export default nextConfig;
+// Configure next-pwa
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // Disable PWA features in development
+});
+
+// Next.js configuration
+export default withPWA({
+  reactStrictMode: true,
+});
